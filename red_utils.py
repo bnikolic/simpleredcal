18484b9e0b0042699efecfea43b75626093b3912
matyasmolnar <mdm49@cam.ac.uk>
2020-04-15 14:43:56 +0100

    Added functions to move between JD and LST

--- red_utils.py
+++ red_utils.py
@@ -1,6 +1,7 @@
 """Utility functions for robust redundant calibration"""
 
 import numpy
+from scipy.optimize import minimize_scalar
 
 import pyuvdata
 from pyuvdata import utils as uvutils
@@ -21,3 +22,16 @@ def jd_to_lst(JD_time, telescope='HERA'):
     lat_lon_alt = pyuvdata.telescopes.get_telescope(telescope).telescope_location_lat_lon_alt_degrees
     lst = uvutils.get_lst_for_time([JD_time], *lat_lon_alt)
     return lst[0]
+
+
+def lst_to_jd_time(lst, JD, telescope='HERA'):
+    """Find the JD time for a JD day that corresponds to a given LST
+
+    """
+    lat_lon_alt = pyuvdata.telescopes.get_telescope(telescope).telescope_location_lat_lon_alt_degrees
+
+    def func(x):
+        return numpy.abs(uvutils.get_lst_for_time([JD+x], *lat_lon_alt)[0] - lst)
+
+    res = minimize_scalar(func, bounds=(0, 1), method='bounded')
+    return res['x']
