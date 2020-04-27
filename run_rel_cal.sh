4a983d31b59eea2507069d779d35fab0f7ec1974
matyasmolnar <mdm49@cam.ac.uk>
2020-04-28 00:15:38 +0100

    submit script edited

--- run_rel_cal.sh
+++ run_rel_cal.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Set PBS Directives
+# Lines starting with "#PBS", before any shell commands are
+# interpreted as command line arguments to qsub.
 # Don't put any commands before the #PBS options or they will not work
+#
 #PBS -V # Export all environment variables from the qsub commands environment to the batch job.
-#PBS -l pmem=64gb,pvmem=64gb # Amount of memory needed by each processor (ppn) in the job.
+#PBS -l pmem=64gb,pvmem=96gb # Amount of memory needed by each processor (ppn) in the job.
 #PBS -d /lustre/aoc/projects/hera/mmolnar/simpleredcal # Working directory (PBS_O_WORKDIR) set to your Lustre area
-#PBS -m ae # Send email when Jobs end or abort
+#PBS -m bea # Send email when Jobs end or abort
+#PBS -l nodes=1:ppn=4 # default is 1 core on 1 node
 
-# casa's python requires a DISPLAY for matplot, so create a virtual X server
 /lustre/aoc/projects/hera/mmolnar/anaconda3/envs/hera/bin/python rel_cal.py 2458098.43869 --pol 'ee'
