0a71b22c6b88af7f6c815c1a3b4196d200fdbe30
matyasmolnar <mdm49@cam.ac.uk>
2021-01-13 12:16:55 +0000

    Reduced memory allocation; JD_time argument changed to str

--- run_rel_cal.sh
+++ run_rel_cal.sh
@@ -6,7 +6,7 @@
 # Don't put any commands before the #PBS options or they will not work
 #
 #PBS -V # Export all environment variables from the qsub commands environment to the batch job.
-#PBS -l mem=128gb,pvmem=192gb # Amount of memory needed by each processor (ppn) in the job.
+#PBS -l mem=64gb,pvmem=96gb # Amount of memory needed by each processor (ppn) in the job.
 #PBS -d /lustre/aoc/projects/hera/mmolnar/simpleredcal # Working directory (PBS_O_WORKDIR) set to your Lustre area
 #PBS -m bea # Send email when Jobs end or abort
 #PBS -l nodes=1:ppn=4 # default is 1 core on 1 node
@@ -14,5 +14,5 @@
 #PBS -j oe
 #PBS -o rel_cal.2458098.43869.ee.gaussian.out
 
-/lustre/aoc/projects/hera/mmolnar/anaconda3/envs/hera/bin/python rel_cal.py 2458098.43869 --pol 'ee' \
---flag_type 'first' --dist 'gaussian'
+/lustre/aoc/projects/hera/mmolnar/anaconda3/envs/hera/bin/python rel_cal.py '2458098.43869' --pol 'ee' \
+--flag_type 'first' --dist 'gaussian' --initp_jd 2458099
