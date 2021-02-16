ef6561326b3b546d9d1d48f6b3521810a5a2e313
matyasmolnar <mdm49@cam.ac.uk>
2021-02-16 09:54:24 -0700

    Better documentation, cleaner printing, ensure that the JD from jd_time arg is included in resulting dataframe, and skip jds for which rel_dfs don't exist

--- align_deg.py
+++ align_deg.py
@@ -1,9 +1,17 @@
 """Degenerately transform, align and merge redundantly calibrated
 dataframes
+
+example run:
+$ python align_deg.py 2458098.43869 --jd_comp '2458098~2458099' \
+--jd_anchor 2458099 --pol 'ee' --dist 'gaussian'
+
+Read the resulting dataframe with:
+> pd.read_pickle('aligned_red_deg.ee.gaussian.pkl')
 """
 
 import argparse
 import datetime
+import glob
 import os
 import pickle
 import textwrap
@@ -64,13 +72,16 @@ def main():
     else:
         jd_comp = mod_str_arg(args.jd_comp)
     jd_comp = numpy.intersect1d(jd_comp, idr2_jds)
+    jdl_day = int(float(args.jd_time))
+    if jdl_day in jd_comp:
+        jd_comp = numpy.delete(jd_comp, numpy.where(jd_comp == jdl_day)[0])
 
     print('Finding relatively calibrated dataframes on JDs {} ({} polarization '\
           'and {} assumed noise during the calibration) and aligning them '\
           'in LAST to dataset {}, with all relatively calibrated datasets being '\
           'transformed to the degenerate space of {}.\n'.\
-          format(numpy.append([int(float(args.jd_time))], jd_comp), args.pol, \
-          args.dist, args.jd_time, args.jd_anchor))
+          format(' '.join(map(str, numpy.sort(numpy.append([jdl_day], jd_comp)))),
+                 args.pol, args.dist, args.jd_time, args.jd_anchor))
 
     if os.path.exists(out_pkl):
         print('Overwriting {}.\n'.format(out_pkl))
@@ -90,9 +101,6 @@ def main():
     rel_df_path = find_rel_df(args.jd_time, args.pol, args.dist, args.rel_dir)
     rel_df = pd.read_pickle(rel_df_path)
     rel_df.drop(columns=resid_cols+gain_list, inplace=True)
-
-    Nfreqs = rel_df.index.get_level_values('freq').unique().size
-    Ntints = rel_df.index.get_level_values('time_int').unique().size
 
     if int(float(args.jd_time)) != args.jd_anchor:
         rel_df_d = rel_df[min_list].copy()
@@ -123,37 +131,43 @@ def main():
     rel_df_d.set_index(new_indices, inplace=True)
     rel_df_d.sort_index(inplace=True)
 
+    avaiable_jds = numpy.unique([os.path.basename(df).split('.')[1] for df in glob.glob(
+        os.path.join(os.getcwd(), args.rel_dir, 'rel_df*pkl'))]).astype(int)
     for jd_ci in jd_comp:
-        rel_dfk = align_df('rel', args.jd_time, jd_ci, args.rel_dir, args.dist, \
-                           args.pol)
-        rel_dfk.drop(columns=resid_cols+gain_list, inplace=True)
+        if jd_ci in avaiable_jds:
+            print('Aligning and adding {} to the resulting dataframe'.format(jd_ci))
+            rel_dfk = align_df('rel', args.jd_time, jd_ci, args.rel_dir, args.dist, \
+                            args.pol)
+            rel_dfk.drop(columns=resid_cols+gain_list, inplace=True)
 
-        if int(jd_ci) != args.jd_anchor:
-            deg_dfk = align_df('deg', args.jd_time, jd_ci, args.deg_dir, args.dist, \
-                               args.pol, JD_anchor=args.jd_anchor)
+            if int(jd_ci) != args.jd_anchor:
+                deg_dfk = align_df('deg', args.jd_time, jd_ci, args.deg_dir, args.dist, \
+                                args.pol, JD_anchor=args.jd_anchor)
 
-            # Degenerate transformation of redundant visibility solutions
-            deg_dfk = deg_dfk[['0', '1', '2']].copy().reset_index()
-            deg_dfk.rename(columns={'time_int1': 'time_int', '0': 'amp', \
-                                    '1': 'tilt_x', '2':'tilt_y'}, inplace=True)
-            deg_dfk.set_index(indices, inplace=True)
-            deg_dfk.sort_index(inplace=True)
-            rel_dfk = rel_dfk.join(deg_dfk)
+                # Degenerate transformation of redundant visibility solutions
+                deg_dfk = deg_dfk[['0', '1', '2']].copy().reset_index()
+                deg_dfk.rename(columns={'time_int1': 'time_int', '0': 'amp', \
+                                        '1': 'tilt_x', '2':'tilt_y'}, inplace=True)
+                deg_dfk.set_index(indices, inplace=True)
+                deg_dfk.sort_index(inplace=True)
+                rel_dfk = rel_dfk.join(deg_dfk)
 
-            rel_df_di = rel_dfk[min_list].copy()
-            rel_df_di = rel_df_di.reindex(columns=rel_df_di.columns.values.tolist() \
-                                                  +vis_list)
-            rel_df_di[vis_list] = rel_dfk.apply(lambda row: pd.Series(decomposeCArray(\
-                degVis(ant_sep, makeCArray(row[len(min_list):len(min_list) + \
-                md['no_unq_bls']*2].values.astype(float)), *row[-3:].values.astype(float)))), \
-                axis=1)
-            rel_dfk = rel_df_di
+                rel_df_di = rel_dfk[min_list].copy()
+                rel_df_di = rel_df_di.reindex(columns=rel_df_di.columns.values.tolist() \
+                                                    +vis_list)
+                rel_df_di[vis_list] = rel_dfk.apply(lambda row: pd.Series(decomposeCArray(\
+                    degVis(ant_sep, makeCArray(row[len(min_list):len(min_list) + \
+                    md['no_unq_bls']*2].values.astype(float)), *row[-3:].values.astype(float)))), \
+                    axis=1)
+                rel_dfk = rel_df_di
 
-        rel_dfk['JD'] = jd_ci
-        rel_dfk.reset_index(inplace=True)
-        rel_dfk.set_index(new_indices, inplace=True)
-        rel_dfk.sort_index(inplace=True)
-        rel_df_d = pd.concat([rel_df_d, rel_dfk])
+            rel_dfk['JD'] = jd_ci
+            rel_dfk.reset_index(inplace=True)
+            rel_dfk.set_index(new_indices, inplace=True)
+            rel_dfk.sort_index(inplace=True)
+            rel_df_d = pd.concat([rel_df_d, rel_dfk])
+        else:
+            print('No relative calibration dataframes available for {}: skipping.'.format(jd_ci))
 
     rel_df_d.sort_index(inplace=True)
     rel_df_d.to_pickle(out_pkl)
