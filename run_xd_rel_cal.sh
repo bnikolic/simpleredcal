a9f88699bde09b91724f6d24382c5872867db7d0
matyasmolnar <mdm49@cam.ac.uk>
2021-03-10 11:50:48 +0000

    Increase walltime, specify band 2 channels, ndist to cauchy

--- run_xd_rel_cal.sh
+++ run_xd_rel_cal.sh
@@ -10,10 +10,10 @@
 #PBS -d /lustre/aoc/projects/hera/mmolnar/simpleredcal # Working directory (PBS_O_WORKDIR) set to your Lustre area
 #PBS -m bea # Send email when Jobs end or abort
 #PBS -l nodes=1:ppn=4 # default is 1 core on 1 node
-#PBS -l walltime=100:00:00
+#PBS -l walltime=200:00:00
 #PBS -j oe
-#PBS -o xd_rel_cal.2458098.43869.ee.gaussian.out
+#PBS -o xd_rel_cal.2458098.43869.ee.cauchy.out
 
 /users/mmolnar/anaconda3/envs/hera/bin/python xd_rel_cal.py '2458098.43869' \
---jds 'idr2_jdsx' --pol 'ee' --flag_type 'first' --dist 'gaussian' \
---out_dir 'xd_rel_dfs'
+--jds 'idr2_jdsx' --pol 'ee' --flag_type 'first' --dist 'cauchy' \
+--out_dir 'xd_rel_dfs' --chans '500~700'
