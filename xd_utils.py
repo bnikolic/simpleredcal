ca06a205413fa9dfa5424c935eb2392bf2ac9112
matyasmolnar <mdm49@cam.ac.uk>
2021-09-07 08:25:58 -0600

    numpy indexing of hd.freqs (instead of jnp, to avoid error) in XDgroup_data

--- xd_utils.py
+++ xd_utils.py
@@ -219,7 +219,7 @@ def XDgroup_data(JD_time, JDs, pol, chans=None, tints=None, bad_ants=True, \
     if rephase:
         lst_bin_centres = np.mean(comb_lsts, axis=0)
 
-        freq_arr = hd.freqs[chans]
+        freq_arr = hd.freqs[numpy.asarray(chans)]
         cData_rph = numpy.empty((JDs_arr.size, freq_arr.size, len(tints), redg.shape[0]), \
                                 dtype=complex)
 
